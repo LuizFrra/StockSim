ac408f5b1b8a84a7337ce9c7b68d5d9bbf83aa92
luiz Alex <luizalex1997@gmail.com>
2020-11-16 13:12:59 -0300

    Add RabbitMQ StartUp

---
+++ src/main/java/com/luizfrra/stockSim/RabbitMQ/RabbitStartUp.java
@@ -0,0 +1,38 @@
+package com.luizfrra.stockSim.RabbitMQ;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitStartUp {
+
+    @Bean("emailToSend")
+    Queue emailToSend() {
+        return new Queue("emailToSend", true, false, false);
+    }
+
+    @Bean("emailHistory")
+    Queue emailHistory() {
+        return new Queue("emailHistory", true, false, false);
+    }
+
+    @Bean
+    FanoutExchange fanoutExchange()  {
+        return new FanoutExchange("stocksim.email", true, false);
+    }
+
+    @Bean
+    Binding bindingEmailOne(@Qualifier("emailToSend") Queue queue, FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(queue).to(fanoutExchange);
+    }
+
+    @Bean
+    Binding bindingEmailTwo(@Qualifier("emailHistory") Queue queue, FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(queue).to(fanoutExchange);
+    }
+}
